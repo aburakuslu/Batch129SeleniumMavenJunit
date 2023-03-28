ba254d7493f0dffdd823765bb6dea658168962c3
aburakuslu <alpburakuslu@gmail.com>
2023-03-28 21:31:33 +0300

    day06

--- src/test/java/day06_Maven/C01_MavenIlkTest.java
+++ src/test/java/day06_Maven/C01_MavenIlkTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class C01_MavenIlkTest {
     public static void main(String[] args) {
@@ -15,9 +17,8 @@ public class C01_MavenIlkTest {
         WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--remote-allow-origins=*"));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
         //https://www.amazon.com/ sayfasina gidelim
-        driver.get("https://www.amazon.com/");
+        driver.get("https://amazon.com");
 
         //arama kutusunu locate edelim
         WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
@@ -27,10 +28,34 @@ public class C01_MavenIlkTest {
         aramaKutusu.submit();
 
         //Bulunan sonuc sayisini yazdiralim
-        WebElement sonucSayisi = driver.findElement(By.xpath(
-                "(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        WebElement sonucSayisi = driver.findElement(By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        String [] sonuc = sonucSayisi.getText().split(" ");
+        System.out.println("Bulunan Sonuc Sayisi = "+sonuc[2]);
+
+        //Lambda ile yazdıralım
+        //1-16 of 223 results for "Samsung headphones"
+        Arrays.stream(sonucSayisi.getText().split(" ")).skip(2).limit(1).
+                forEach(t-> System.out.println("Sonuc Sayisi "+t));
+
+        Arrays.stream(sonucSayisi.getText().split(" ")).limit(3).skip(2).
+                forEach(t-> System.out.println("Sonuc Sayisi2 = "+t));
+
+
+
         //Ilk urunu tiklayalim
-        
+        driver.findElement(By.xpath("(//*[@class='a-link-normal s-underline-text " +
+                "s-underline-link-text s-link-style a-text-normal'])[1]")).click();
+
         //Sayfadaki tum basliklari yazdiralim
+        List<WebElement> sayfaBasliklari = driver.findElements(By.xpath("//h1"));
+        for (WebElement w:sayfaBasliklari) {
+            System.out.println(w.getText());
+        }
+        //Lambda ile yazalım
+        sayfaBasliklari.forEach(t-> System.out.println(t.getText()));
+
+        //Sayfayı kapatını
+        driver.close();
+
     }
 }

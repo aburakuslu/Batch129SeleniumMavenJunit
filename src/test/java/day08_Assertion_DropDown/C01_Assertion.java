b1739a58e777a6d12efc681ab696974f43c87187
aburakuslu <alpburakuslu@gmail.com>
2023-03-31 00:12:18 +0300

    day08

---
+++ src/test/java/day08_Assertion_DropDown/C01_Assertion.java
@@ -0,0 +1,72 @@
+package day08_Assertion_DropDown;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_Assertion {
+    /*
+ 1)https://amazon.com adresine gidin
+ 2)Aşağıdaki adları kullanarak 4 test metodu oluşturun ve gerekli testleriyapin
+ ○ title Test  => Sayfa başlığının “Amazon” kelimesini içerip içermediğini test edin
+ ○ image Test => Amazon resminin görüntülendiğini (isDisplayed()) test edin
+ ○ Search Box 'in erisilebilir oldugunu test edin(isEnabled())
+ ○ wrongTitleTest => Sayfa basliginin “amazon” içermediğini doğrulayın
+     */
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://amazon.com");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);
+        driver.close();
+    }
+
+    // ○ title Test  => Sayfa başlığının “Amazon” kelimesini içerip içermediğini test edin
+    @Test
+    public void titleTest() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Amazon";
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+    }
+
+    // ○ image Test => Amazon resminin görüntülendiğini (isDisplayed()) test edin
+    @Test
+    public void imageTest() {
+        WebElement logo = driver.findElement(By.id("nav-logo-sprites"));
+        Assert.assertTrue(logo.isDisplayed());
+    }
+
+    // ○ Search Box 'in erisilebilir oldugunu test edin(isEnabled())
+    @Test
+    public void searchBox() {
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        Assert.assertTrue(searchBox.isEnabled());
+    }
+
+    // ○ wrongTitleTest => Sayfa basliginin “amazon” içermediğini doğrulayın
+    @Test
+    public void wrongTitleTest() {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "amazon";
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+        // Assert.assertFalse(driver.getTitle().contains("amazon"));
+    }
+}
